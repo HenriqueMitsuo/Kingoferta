ae54676926f5f172fa91991d6bf5a55b5218e8b6
MoisesDuarte <moisesduartejunior@gmail.com>
2019-08-12 15:53:53 -0300

    Corrigindo alguns erros e implementando logo do app em components/AppBar.vue

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -9,17 +9,23 @@
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" class="deep-purple" app>
-        <v-list-item to="/" dark>
+        <v-list-item>
             <v-list-item-content>
-                <v-list-item-tile>Home</v-list-item-tile>
+                <v-img src="..\assets\Logo_KingOferta.png"></v-img>
             </v-list-item-content>
         </v-list-item>
-
+        <v-list-item-group color="deep-purple accent-1">
+            <v-list-item to="/" dark>
+            <v-list-item-content>
+                <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
         <v-list-item to="/about" dark>
             <v-list-item-content>
-                <v-list-item-tile router:to="/about">About</v-list-item-tile>
+                <v-list-item-title router:to="/about">About</v-list-item-title>
             </v-list-item-content>
         </v-list-item>
+        </v-list-item-group>
     </v-navigation-drawer>
 
   </div>
