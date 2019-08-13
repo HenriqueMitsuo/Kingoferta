1cedc412ccb63cc83035d4ac9b9b24a6c0f8e826
HenriqueMitsuo <henrique.informatica123@gmail.com>
2019-08-13 19:28:02 -0300

    Limpeza de layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="home">
-    <HelloWorld />
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
