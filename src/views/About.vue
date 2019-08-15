7eda589cf2e0dfeff5b11ec5b91f7b78ed5970bb
MoisesDuarte <moisesduartejunior@gmail.com>
2019-08-14 21:59:16 -0300

    Definindo layout da tela about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-list class="text-center" three-line>
+      <v-avatar size="128">
+        <img src="../assets/Logo_KingOferta.png" alt="avatar">
+      </v-avatar>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="headline mb-1"><span class="font-weight-light">King</span><span>Ofertas</span></v-list-item-title>
+          <v-list-item-subtitle>Aplicativo para cotação de preços</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <v-divider/>
+
+    <v-list two-line flat>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Versão</v-list-item-title>
+          <v-list-item-subtitle>1.0.0</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <v-divider/>
+
+    <v-list flat subheader>
+      <v-subheader>Contate-nos</v-subheader>
+    </v-list>
+
+
+
   </div>
 </template>
