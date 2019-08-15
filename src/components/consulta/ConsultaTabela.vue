94a7bdafa9c2e19455c1f2443f9e486ef1d52a73
HenriqueMitsuo <henrique.informatica123@gmail.com>
2019-08-15 16:11:22 -0300

    Adicionado primeira implementação dos Filtros

--- src/components/consulta/ConsultaTabela.vue
+++ src/components/consulta/ConsultaTabela.vue
@@ -22,120 +22,43 @@
 
     <v-data-table
       :headers="headers"
-      :items="desserts"
-      :search="filters">
+      :items="filterName">
     </v-data-table>
   </v-card>
 </template>
 
 <script>
+import Placeholder from "./get.json"
 export default {
     name: 'ConsultaTabela',
     data() {
-            return {
-                filters: { name: '', calories: '' },
-                headers: [
-                    { text: 'Dessert (100g serving)', align: 'left', sortable: false, value: 'name' },
-                    { text: 'Calories', value: 'calories' },
-                    { text: 'Fat (g)', value: 'fat' },
-                    { text: 'Carbs (g)', value: 'carbs' },
-                    { text: 'Protein (g)', value: 'protein' },
-                    { text: 'Iron (%)', value: 'iron' },
-                ],
-                desserts: [
-                    
-                    {
-                        name: 'Frozen Yogurt',
-                        calories: 159,
-                        fat: 6.0,
-                        carbs: 24,
-                        protein: 4.0,
-                        iron: '1%',
-                    },
-                    {
-                        name: 'Frozen Yogurt',
-                        calories: 123,
-                        fat: 6.0,
-                        carbs: 24,
-                        protein: 4.0,
-                        iron: '1%',
-                    },
-                    {
-                        name: 'Ice cream sandwich',
-                        calories: 237,
-                        fat: 9.0,
-                        carbs: 37,
-                        protein: 4.3,
-                        iron: '1%',
-                    },
-                    {
-                        name: 'Eclair',
-                        calories: 262,
-                        fat: 16.0,
-                        carbs: 23,
-                        protein: 6.0,
-                        iron: '7%',
-                    },
-                    {
-                        name: 'Cupcake',
-                        calories: 305,
-                        fat: 3.7,
-                        carbs: 67,
-                        protein: 4.3,
-                        iron: '8%',
-                    },
-                    {
-                        name: 'Gingerbread',
-                        calories: 356,
-                        fat: 16.0,
-                        carbs: 49,
-                        protein: 3.9,
-                        iron: '16%',
-                    },
-                    {
-                        name: 'Jelly bean',
-                        calories: 375,
-                        fat: 0.0,
-                        carbs: 94,
-                        protein: 0.0,
-                        iron: '0%',
-                    },
-                    {
-                        name: 'Lollipop',
-                        calories: 392,
-                        fat: 0.2,
-                        carbs: 98,
-                        protein: 0,
-                        iron: '2%',
-                    },
-                    {
-                        name: 'Honeycomb',
-                        calories: 408,
-                        fat: 3.2,
-                        carbs: 87,
-                        protein: 6.5,
-                        iron: '45%',
-                    },
-                    {
-                        name: 'Donut',
-                        calories: 452,
-                        fat: 25.0,
-                        carbs: 51,
-                        protein: 4.9,
-                        iron: '22%',
-                    },
-                    {
-                        name: 'KitKat',
-                        calories: 518,
-                        fat: 26.0,
-                        carbs: 65,
-                        protein: 7,
-                        iron: '6%',
-                    },
-                ],
-            }
+        return {
+            filters: { name: '', calories: '' },
+            search: 'tesete',
+            headers: [
+                { text: 'Dessert (100g serving)', align: 'left', sortable: false, value: 'name' },
+                { text: 'Calories', value: 'calories' },
+                { text: 'Fat (g)', value: 'fat' },
+                { text: 'Carbs (g)', value: 'carbs' },
+                { text: 'Protein (g)', value: 'protein' },
+                { text: 'Iron (%)', value: 'iron' },
+            ],
+            desserts: [],
         }
-    }
+    },
+    created () {
+        this.desserts = Placeholder;
+    },
+    computed: {
+        // https://front.id/en/articles/vuetify-achieve-multiple-filtering-data-table-component
+        // https://www.youtube.com/watch?v=KSHmxn2fgv0
+        filterName: function(){
+            return this.desserts.filter((dessert) => {
+                return dessert.name.match(this.filters.name)
+            });
+        },
+    },
+}
 </script>
 
 <style>
