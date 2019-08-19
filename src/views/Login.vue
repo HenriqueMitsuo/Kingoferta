ed0b57852085b9d90a286eb078bcc82fef7ac593
MoisesDuarte <moisesduartejunior@gmail.com>
2019-08-19 13:28:16 -0300

    Definido layout inicial do Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>Login</h1>
+  <div class="login">
+        <v-form v-model="valid">
+          <v-container>
+            <v-text-field v-model="email" :rules="emailRules" label="Email" prepend-inner-icon="mdi-email" filled required></v-text-field>
+            <v-text-field v-model="password" :type="showPass ? 'text' : 'password'" :rules="passwordRules" label="Senha" :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'" prepend-inner-icon="mdi-key" @click:append="showPass = !showPass" filled required></v-text-field>
+            <v-btn large outlined block tile color="primary"><v-icon left>mdi-login</v-icon>Entrar</v-btn>
+          </v-container>
+        </v-form>
+  </div>
 </template>
 
 <script>
 export default {
-
+    data: () => (
+        {
+            valid: false,
+            showPass: false,
+            email: '',
+            password: '',
+            emailRules: [
+                v => !!v || 'Email é necessário',
+                v => /.@.+/.test(v) || 'Email inválido'
+            ]
+        }
+    ),
 }
 </script>
 
