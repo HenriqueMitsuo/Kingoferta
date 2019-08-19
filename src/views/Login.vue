326592bb5dc86ae7ee2a4c54c6e3c35cc55c73de
MoisesDuarte <moisesduartejunior@gmail.com>
2019-08-19 13:33:19 -0300

    Correções em LoginLayout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="login">
-        <v-form v-model="valid">
-          <v-container>
-            <v-text-field v-model="email" :rules="emailRules" label="Email" prepend-inner-icon="mdi-email" filled required></v-text-field>
-            <v-text-field v-model="password" :type="showPass ? 'text' : 'password'" :rules="passwordRules" label="Senha" :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'" prepend-inner-icon="mdi-key" @click:append="showPass = !showPass" filled required></v-text-field>
-            <v-btn large outlined block tile color="primary"><v-icon left>mdi-login</v-icon>Entrar</v-btn>
-          </v-container>
-        </v-form>
-  </div>
+    <div class="login">
+        <v-container>
+            <v-form v-model="valid">
+                <v-text-field v-model="email" :rules="emailRules" label="Email" prepend-inner-icon="mdi-email" filled required></v-text-field>
+                <v-text-field v-model="password" :type="showPass ? 'text' : 'password'" :rules="passwordRules" label="Senha" :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'" prepend-inner-icon="mdi-key" @click:append="showPass = !showPass" filled required></v-text-field>
+                <v-btn large outlined block tile color="primary"><v-icon left>mdi-login</v-icon>Entrar</v-btn>
+            </v-form>
+        </v-container>
+    </div>
 </template>
 
 <script>
