ed5195637dd8d3f77034745c1fb8988aaa98f470
MoisesDuarte <moisesduartejunior@gmail.com>
2019-08-19 14:47:53 -0300

    Transformando login em um dialog

---
+++ src/components/login/LoginLayout.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-card class="primary justify center" dark>
+        <v-app-bar flat dark color="primary">
+            <v-btn icon dark v-on:click="closeDialog"><v-icon>mdi-close</v-icon></v-btn>
+        </v-app-bar>
+        <v-container>
+            <v-form v-model="valid">
+                <v-text-field v-model="email" :rules="emailRules" label="Email" prepend-inner-icon="mdi-email" outlined required></v-text-field>
+                <v-text-field v-model="password" :type="showPass ? 'text' : 'password'"  label="Senha" :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'" prepend-inner-icon="mdi-key" @click:append="showPass = !showPass" outlined required></v-text-field>
+                <v-btn depressed large block tile color="green"><v-icon left>mdi-login</v-icon>Entrar</v-btn>
+            </v-form>
+        </v-container>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data: () => (
+        {
+            valid: false,
+            showPass: false,
+            email: '',
+            password: '',
+            emailRules: [
+                v => !!v || 'Email é necessário',
+                v => /.@.+/.test(v) || 'Email inválido'
+            ]
+        }
+    ),
+    methods: {
+        closeDialog : function() {
+            console.log('close dialog');
+            this.$emit('close-dialog')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,25 @@
 <template>
     <div class="login">
-        <v-container>
-            <v-form v-model="valid">
-                <v-text-field v-model="email" :rules="emailRules" label="Email" prepend-inner-icon="mdi-email" filled required></v-text-field>
-                <v-text-field v-model="password" :type="showPass ? 'text' : 'password'" :rules="passwordRules" label="Senha" :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'" prepend-inner-icon="mdi-key" @click:append="showPass = !showPass" filled required></v-text-field>
-                <v-btn large outlined block tile color="primary"><v-icon left>mdi-login</v-icon>Entrar</v-btn>
-            </v-form>
-        </v-container>
+        <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+            <template v-slot:activator="{ on }"><v-btn color="primary" dark v-on="on">Open Dialog</v-btn></template>                  
+            <LoginLayout v-on:close-dialog="dialog = false"/> 
+        </v-dialog>     
     </div>
 </template>
 
 <script>
+import LoginLayout from '../components/login/LoginLayout'
+
 export default {
-    data: () => (
-        {
-            valid: false,
-            showPass: false,
-            email: '',
-            password: '',
-            emailRules: [
-                v => !!v || 'Email é necessário',
-                v => /.@.+/.test(v) || 'Email inválido'
-            ]
+    name: 'login',
+    components: {
+        LoginLayout,
+    },
+    data () {
+        return {
+            dialog: false,
         }
-    ),
+    }
 }
 </script>
 
