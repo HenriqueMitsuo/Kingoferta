6fd14c94f6aaf1b707e8e789aa7dec0c0e194333
MoisesDuarte <moisesduartejunior@gmail.com>
2019-08-20 11:01:21 -0300

    Definindo layout de registro de dados

---
+++ src/components/registrar/DadosLayout.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="dados">
+        <v-text-field v-model="name" label="Nome" prepend-inner-icon="mdi-account" filled required />    
+        <v-dialog ref="dialog" v-model="modal" :return-value.sync="date" persistent full-width width="290px">
+            <template v-slot:activator="{ on }"><v-text-field v-model="date" label="Data de nascimento" prepend-inner-icon="mdi-calendar-range" filled readonly v-on="on"/></template>
+            <v-date-picker v-model="date" scrollable>
+                <v-spacer/>
+                <v-btn text color="primary" @click="modal = false">Cancelar</v-btn>
+                <v-btn text color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
+            </v-date-picker>            
+        </v-dialog>
+        <v-combobox v-model="select" :items="items" label="Gênero" prepend-inner-icon="mdi-human-male-female" filled required/>
+        <v-text-field v-model="cellphone" label="Celular" prepend-inner-icon="mdi-contact-phone" filled required />
+        <v-text-field v-model="password" label="Senha" filled prepend-inner-icon="mdi-key"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DadosLayout',
+    data: () => ({
+        date: new Date().toISOString().substr(0, 10),
+        modal: false,
+        select: 'Selecionar',
+        items: [
+            'Masculino',
+            'Feminino',
+        ]
+    }),
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Registrar.vue
+++ src/views/Registrar.vue
@@ -1,31 +1,29 @@
 <template>
   <div>
-      <v-stepper v-model="e1">
+      <v-stepper dark class="primary elevation-0" v-model="e1">
 
-          <v-stepper-header>
-              <v-stepper-step :complete="e1 > 1" step="1">Registrar dados</v-stepper-step>
+          <v-stepper-header class="elevation-0">
+              <v-stepper-step color="accent" :complete="e1 > 1" step="1">Registrar dados</v-stepper-step>
               <v-divider/>
-              <v-stepper-step :complete="e1 > 2" step="2">Responder pesquisa</v-stepper-step>
+              <v-stepper-step color="accent" :complete="e1 > 2" step="2">Responder pesquisa</v-stepper-step>
               <v-divider/>
-              <v-stepper-step step="3">Concluir cadastro</v-stepper-step>
+              <v-stepper-step color="accent" step="3">Concluir cadastro</v-stepper-step>
           </v-stepper-header>
+
+          <v-divider/>
 
           <v-stepper-items>
               <v-stepper-content step="1">
-                  <h1>Registrar Dados</h1>
-                  <v-btn color="primary" @click="e1 = 2">Continuar</v-btn>
-                  <v-btn text>Cancelar</v-btn>
+                 <DadosLayout />        
               </v-stepper-content>
 
               <v-stepper-content step="2">
-                  <h1>Responder pesquisa</h1>
                   <v-btn color="primary" @click="e1 = 3">Continuar</v-btn>
                   <v-btn text>Cancelar</v-btn>
               </v-stepper-content>
 
               <v-stepper-content step="3">
-                  <h1>Concluir cadastro</h1>
-                  <v-btn color="primary">Continuar</v-btn>
+                  <v-btn color="primary">Concluir</v-btn>
                   <v-btn text>Cancelar</v-btn>
               </v-stepper-content>
 
@@ -36,11 +34,16 @@
 </template>
 
 <script>
+import DadosLayout from '../components/registrar/DadosLayout'
+
 export default {
     data () {
         return {
             e1: 0,
         }
+    },
+    components: {
+        DadosLayout,
     },
 }
 </script>
