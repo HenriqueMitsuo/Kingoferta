a654fa789d15bd63dd7e296df1dc86942b010d45
MoisesDuarte <moisesduartejunior@gmail.com>
2019-08-19 22:08:19 -0300

    Revisando LayoutLogin.vue

--- src/components/login/LoginLayout.vue
+++ src/components/login/LoginLayout.vue
@@ -10,9 +10,12 @@
         </v-row>
         <v-container class="pa-6">          
             <v-form v-model="valid">
-                <v-text-field v-model="email" :rules="emailRules" label="Email" prepend-inner-icon="mdi-email" solo required></v-text-field>
-                <v-text-field v-model="password" :type="showPass ? 'text' : 'password'"  label="Senha" :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'" prepend-inner-icon="mdi-key" @click:append="showPass = !showPass" solo required></v-text-field>
-                <v-btn rounded large block color="green" dark><v-icon left>mdi-login</v-icon>Entrar</v-btn>
+                <v-text-field v-model="email" :rules="emailRules" label="Email" prepend-inner-icon="mdi-email" outlined required></v-text-field>
+                <v-text-field v-model="password" :type="showPass ? 'text' : 'password'"  label="Senha" :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'" prepend-inner-icon="mdi-key" @click:append="showPass = !showPass" outlined required></v-text-field>
+                <div class="mb-2">
+                    <v-btn rounded large block color="green" dark><v-icon left>mdi-login</v-icon>Entrar</v-btn>
+                </div>
+                <v-btn rounded large block color="secondary" dark><v-icon left>mdi-pencil</v-icon>Cadastrar</v-btn>
             </v-form>
         </v-container>
     </v-card>
