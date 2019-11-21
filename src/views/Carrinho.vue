687f9e306e4983c639dd5cae1a56ea52ae10e9d4
HenriqueMitsuo <henrique.informatica123@gmail.com>
2019-11-20 21:30:10 -0300

    Layout inicial do carrinho

--- src/views/Carrinho.vue
+++ src/views/Carrinho.vue
@@ -1,5 +1,22 @@
 <template>
-  <div></div>
+    <v-card>
+        <v-container >
+        <v-row >
+            <v-col cols="6">
+                <v-checkbox label="teste - 1"></v-checkbox>
+            </v-col>
+            <v-col  cols="6">
+                <v-checkbox label=""></v-checkbox>
+            </v-col>
+            <v-col  cols="6">
+                <v-checkbox label="teste - 3"></v-checkbox>
+            </v-col>
+            <v-col  cols="6">
+                <v-checkbox label=""></v-checkbox>
+            </v-col>
+        </v-row>
+    </v-container>
+    </v-card>
 </template>
 
 <script>
