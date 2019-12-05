9adf6e091127765b857662c85b9979f66ee058ce
MoisesDuarte <moisesduartejunior@gmail.com>
2019-12-05 15:53:46 -0300

    Otimizando Layout Home + Adicionando dialog para mensagem de curso

---
+++ src/components/home/MessageDialog.vue
@@ -0,0 +1,26 @@
+<template>
+   <v-dialog v-model="dialog" persistent max-width="290">
+      <v-card>
+        <v-card-title class="headline">Título</v-card-title>
+        <v-card-text>Corpo</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="red darken-1" text @click="dialog = false">Fechar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            dialog: true,
+        }   
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,70 +1,17 @@
 <template>
   <div class="home">
-    <v-carousel height="300" cycle :hide-delimiters='true'>
-      <v-carousel-item :src="image.url"  v-for="(image) in courseImages" :key="image.order" />    
-    </v-carousel>
-    <v-container class="pr-0 pl-0" fluid>
-      <v-row>
-        <v-col v-for="statistic in statistics" :key="statistic.text" cols="6">
-          <v-card class="primary" outlined dark>
-              <v-list-item four-line>
-                <v-list-item-content class="align-self-end">
-                  <v-list-item-title class="headline mb-2">{{ statistic.quantity }}</v-list-item-title>
-                  <v-list-item-subtitle class="overline">{{ statistic.text }}</v-list-item-subtitle>         
-                </v-list-item-content>               
-                <v-icon class="align-self-end">{{ statistic.icon }}</v-icon>
-              </v-list-item>
-              <v-card-actions></v-card-actions>
-          </v-card>   
-        </v-col>
-      </v-row>
-    </v-container>
-
-    <v-btn color="primary" to="/consulta/produto/1" block><v-icon>mdi-magnify</v-icon> Procurar</v-btn>
+    <MessageDialog/>
+    <HomeLayout/>
   </div>
 </template>
+
 <script>
-import Axios from 'axios'
-
+import HomeLayout from '../components/home/HomeLayout'
+import MessageDialog from '../components/home/MessageDialog'
 export default {
-  name: 'home',
-  data() {
-    return {
-      courseImages: [],
-      preferredCourse: '',
-      isGraduated: '',
-      route: '/consulta/produto/2',
-      statistics: [
-        {icon: 'mdi-city-variant', quantity: '7', text: "Cidades"},
-        {icon: 'mdi-store', quantity: '14', text: "Lojas"},
-        {icon: 'mdi-baguette', quantity: '220', text: "Produtos"},
-        {icon: 'mdi-cash-usd', quantity: '7', text: "Cotações"},
-      ],
+    components: {
+        HomeLayout,
+        MessageDialog
     }
-  },
-  created() {
-    this.preferredCourse = localStorage.getItem('preferredCourse');
-    this.isGraduated = localStorage.getItem('isGraduated');
-
-    let data = JSON.stringify({
-      preferredCourse: this.preferredCourse,
-      isGraduated: this.isGraduated
-    });
-
-    // FETCH DE IMAGENS (lógica funcionando, falta arrumar o cors)
-    const url = 'http://unisepe-cotacao.gearhostpreview.com/pst_api/consultaImagens.php';
-    Axios.post(url, data)
-    .then((Response) => {
-      let i = 0;
-      while(i < Response.data.length) {
-        let url = {
-          url:  Response.data[i]
-        };
-        this.courseImages.push(url);
-        i++;
-      }
-    });
-
-  }
 }
 </script>
