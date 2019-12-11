c8aa21ce4ce9cb8c8d0e53f9dbf3ab34acb45873
MoisesDuarte <moisesduartejunior@gmail.com>
2019-12-11 14:00:16 -0300

    Adicionando placeholder

--- src/components/home/MessageDialog.vue
+++ src/components/home/MessageDialog.vue
@@ -1,8 +1,11 @@
 <template>
-   <v-dialog v-model="dialog" persistent max-width="290">
+   <v-dialog v-model="dialog" persistent max-width="300">
       <v-card>
-        <v-card-title class="headline">Título</v-card-title>
-        <v-card-text>Corpo</v-card-text>
+        <v-card-title class="headline">Vestibular 2020</v-card-title>
+        <v-card-text> 
+          As chamadas para o vestibular de 2020 já estão disponíveis no site da unisepe pelo seguinte endereço: 
+          <br> <a href="http://portal.unisepe.com.br/univr/mural/vestibular-2020-chamada/">http://portal.unisepe.com.br/univr/mural/vestibular-2020-chamada/</a> 
+        </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="red darken-1" text @click="dialog = false">Fechar</v-btn>
