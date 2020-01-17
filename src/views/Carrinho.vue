97ce9fe0af7b0bf4b6faebfd131372d4dc141762
HenriqueMitsuo <henrique.informatica123@gmail.com>
2020-01-17 14:51:09 -0300

    AJustes no carrinho de compras

--- src/views/Carrinho.vue
+++ src/views/Carrinho.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <v-card >
-            <v-card-title class="headline grey--text text--lighten-4 indigo darken-1">Carrinho de produtos</v-card-title>
+            <v-card-title class="headline grey--text text--lighten-4 indigo darken-1">carrinhos de produtos</v-card-title>
                 <div>
                     <v-card-text>
                         <v-autocomplete
@@ -24,8 +24,8 @@
 
         <v-divider class="py-2"></v-divider>
 
-        <v-card v-if="carrinho">
-            <v-card-title class="headline grey--text text--lighten-4 indigo lighten-2">Carrinho - Geral</v-card-title>
+        <!-- <v-card v-if="carrinhos">
+            <v-card-title class="headline grey--text text--lighten-4 indigo lighten-2">carrinhos - Geral</v-card-title>
             <div>
                 <v-simple-table>
                     <template v-slot:default>
@@ -37,9 +37,9 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="produto in carrinho" :key="produto.nome">
+                            <tr v-for="produto in carrinhos" :key="produto.nome">
                                 <td>{{ produto.nome }}</td>
-                                <!-- @TODO: ajustar a nomeclatura padrão -->
+                                
                                 <td>{{ produto.estabelecimento }}</td>
                                 <td>{{ produto.valor }}</td>
                             </tr>
@@ -47,19 +47,19 @@
                     </template>
                 </v-simple-table>
             </div>
-        </v-card>
+        </v-card> -->
         <v-sheet tile elevation="7" color="primary" class="mt-1 mb-12">
                 <!-- <p class="text-center subtitle-1 grey--text text--lighten-3"></p> -->
         </v-sheet>
 
         <v-expansion-panels accordion inset>
-            <v-expansion-panel v-for="estabelecimentosNome in estabelecimentosNomes" :key="estabelecimentosNome.total">
+            <v-expansion-panel v-for="(carrinho, index) in carrinhos" :key="index">
                 <v-expansion-panel-header class="indigo lighten-1">
-                    <p class="title font-weight-bold text-center grey--text text--lighten-3">Carrinho {{estabelecimentosNome.estabelecimento_nome}}</p>
+                    <p class="title font-weight-bold text-center grey--text text--lighten-3">carrinho {{carrinho[0]}}</p>
                 </v-expansion-panel-header>
                 <v-expansion-panel-content>
-                    <v-card v-if="carrinho" class="mt-4">
-                        <v-card-title class="headline grey--text text--lighten-4 indigo lighten-2">Produtos - {{estabelecimentosNome.estabelecimento_nome}}</v-card-title>
+                    <v-card v-if="carrinhos" class="mt-4">
+                        <v-card-title class="headline grey--text text--lighten-4 indigo lighten-2">Produtos - {{carrinho[0]}}</v-card-title>
                         <div>
                             <v-simple-table>
                                 <template v-slot:default>
@@ -70,8 +70,8 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr v-for="produto in estabelecimentos" 
-                                        v-if="produto.estabelecimento == estabelecimentosNome.estabelecimento_nome" :key="produto.valor" >
+                                        <tr v-for="(produto, index) in carrinho[2]" 
+                                        :key="index" >
                                             <td>{{ produto.nome }}</td>
                                             <td>{{ produto.valor }}</td>
                                         </tr>
@@ -81,7 +81,7 @@
                         </div>
                     </v-card>
                     <v-sheet tile elevation="7" color="primary" class="mt-1">
-                            <p class="text-center subtitle-1 grey--text text--lighten-3"> {{estabelecimentosNome.estabelecimento_nome}} -  Total - R$ {{estabelecimentosNome.total}}</p>
+                            <p class="text-center subtitle-1 grey--text text--lighten-3"> {{carrinho[0]}} -  Total - R$ {{carrinho[1]}}</p>
                     </v-sheet>
                 </v-expansion-panel-content>
             </v-expansion-panel>
@@ -97,10 +97,10 @@ export default {
         return {
             loading: false,
             filtro: [],
-            produtos: ['Bisteca Bovina', 'Arroz', 'Feijão', 'Óleo de soja', 'Açúcar', 'Sal', 'Café', 'Molho de tomate', 'Macarrão'],
-            carrinho: [],
-            estabelecimentos: [],
-            estabelecimentosNomes: [],
+            produtos: ['Bisteca Bovina', 'Acetato de Dexamentasona', 'Arroz', 'Feijão Carioca', 'Óleo de soja', 'Açúcar', 'Sal', 'Café', 'Molho de tomate', 'Macarrão'],
+            carrinhos: [],
+            estabelecimentoDados: [],
+            estabelecimentoProdutos: [],
         }
     },
     methods: {
@@ -108,38 +108,22 @@ export default {
         fetchProdutos: async function() {
 
             this.loading = !this.loading;
-
-            const urlProdutos = 'http://cotapreco.iesep.com.br/pst_api/consultacarrinho.php';
+            
+            const urlProdutos = 'http://cotapreco.iesep.com.br/pst_api/consultaCarrinho.php';
             const urlEstabelecimentos = 'http://cotapreco.iesep.com.br/pst_api/consultaestabelecimentos.php';
 
             const data = JSON.stringify({
                 postFiltro: this.filtro
             });
 
-            // @FETCH CARRINHO GERAL
+            // @FETCH carrinhos
             await Axios.post(urlProdutos, data)
                 .then(Response => {
 
-                    return this.carrinho = Response.data;
+                    return this.carrinhos = Response.data;
                 });
 
-            // @FETCH CARRINHO ESTABELECIMENTOS
-            await Axios.post(urlEstabelecimentos, data)
-                .then(Response => {
-                    
-                    let teste = [];
-                    let teste1 = [];
-
-                    teste = Response.data;
-                    teste1 = Response.data;
-
-                    this.estabelecimentosNomes = teste1.pop();
-                    this.estabelecimentos = teste;
-                    //this.estabelecimentos.pop();
-                });
-
-            console.log(this.estabelecimentos);
-            console.log(this.estabelecimentosNomes);
+                console.log(this.carrinhos);
 
             this.loading = !this.loading;
         }
